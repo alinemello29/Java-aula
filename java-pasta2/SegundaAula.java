4f52c7bb407e11b3918a78021b44985352df9dc2
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2024-01-01 19:29:12 -0300

    Add files via upload

---
+++ java-pasta2/SegundaAula.java
@@ -0,0 +1,38 @@
+package br.com.vainaweb;
+
+public class SegundaAula {
+	 public static void main (String[] args) {
+		   
+	     //EXEMPLO DE NUMEROS INTEIROS
+	     byte exemplo1 = 127; // o menor dos numeros
+	     short exemplo2 = 32767; // a Sequencia
+	     int exemplo3 = 2147483647;// o mais comum
+	     long exemplo4 = 9842391221599412L;// o que aceita mais
+	     
+	     //EXEMPLO DE NUMEROS REAIS
+	     float num1 = 5.50f; // permite 7 digitos após o ponto
+	     double num2 = 10.40; // permite 15 digitos após o ponto
+	     
+	     //EXEMPLO DE CARACTERES
+	     char letra = 'S'; // sua inicialização precisa obrigatoriamente ser com aspas simples.
+	    //Aceita dados da tabela ASCII, como exemplo -> o numero 97 = 'a'
+	    
+	    //EXEMPLO DE VALOR BOOLEANO
+	    boolean sim = true; //somente true ou false
+	    
+	    System.out.println(exemplo1);
+	    System.out.println(exemplo2);
+	    System.out.println(exemplo3);
+	    System.out.println(exemplo4);
+	    System.out.println(num1);
+	    System.out.println(num2);
+	    System.out.println(letra);
+	    System.out.println(sim);
+	    
+	    //tarefinha pra casa
+	    //Criar uma listinha de impressão utilizando todos os tipos primitivos.
+	    //27-10-2023 tipos primitivos e variaveis 
+	   
+	   }
+
+}
